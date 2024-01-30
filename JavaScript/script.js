175edcffb4d7ad4bf85122295376105c2fe65ed2
SGabor <csajbok.attila.csaba.2020i@bankitatabanya.hu>
2024-01-30 10:34:01 +0100

    ResponsiveTable0.1

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -150,3 +150,13 @@ var availableTags = [
 $( ".tags" ).autocomplete({
   source: availableTags
 });
+
+
+
+$(document).ready(function() {
+  var table = $('#table').DataTable( {
+      responsive: true
+  } );
+
+  new $.fn.dataTable.FixedHeader( table );
+} );
